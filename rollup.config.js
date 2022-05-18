0038e267ec5cc63ddbeb5022a1d56e39320d4b4a
fuzhenn <fuzhen@maptalks.org>
2022-05-18 12:01:41 +0800

    update build flow

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+const pkg = require('./package.json');
+const { babel } = require('@rollup/plugin-babel');
+
+const banner = `/*!\n * ${pkg.name} v${pkg.version}\n * LICENSE : ${pkg.license}\n * (c) 2016-${new Date().getFullYear()} maptalks.com\n */`;
+
+const plugins = [babel({ babelHelpers: 'bundled' })];
+const external = ['maptalks', 'echarts'];
+const globals = {
+    'maptalks': 'maptalks',
+    'echarts': 'echarts'
+};
+module.exports = [
+    {
+        input: 'index.js',
+        plugins,
+        external,
+
+        output: [
+            {
+                'sourcemap': false,
+                'format': 'es',
+                'banner': banner,
+                globals,
+                'file': pkg.module
+            },
+            {
+                'sourcemap': false,
+                'format': 'umd',
+                'banner': banner,
+                'name': 'maptalks',
+                globals,
+                'file': pkg.main
+            },
+            {
+                'sourcemap': false,
+                'format': 'umd',
+                'banner': banner,
+                'name': 'maptalks',
+                globals,
+                'file': 'dist/maptalks.e3.min.js'
+            }
+        ]
+    }
+];
